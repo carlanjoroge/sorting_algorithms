c0134ab713d05193ec877fff12f8999938b0f1a9
carlanjoroge <carlanjoroge16@gmail.com>
2023-09-26 21:55:34 +0300

    selection and quick sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_ints - two integers are swappped in an array.
+ * @a: integer one.
+ * @b: integer two.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - integer array is sorted in an ascending order
+ *                  using selection sort algorithm.
+ * @array: An integer array.
+ * @size: size of array.
+ *
+ * Description: Prints array after every swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
